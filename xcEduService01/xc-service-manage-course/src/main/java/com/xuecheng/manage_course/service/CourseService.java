c815cd0d4fa56435b97576fae49d574053535666
liusanmu <13053047567@163.com>
2019-08-15 18:21:45 +0800

    first tijiao

---
+++ xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
@@ -0,0 +1,150 @@
+package com.xuecheng.manage_course.service;
+
+import com.xuecheng.framework.domain.course.CourseBase;
+import com.xuecheng.framework.domain.course.CoursePic;
+import com.xuecheng.framework.domain.course.Teachplan;
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_course.dao.CourseBaseRepository;
+import com.xuecheng.manage_course.dao.CoursePicRepository;
+import com.xuecheng.manage_course.dao.TeachplanMapper;
+import com.xuecheng.manage_course.dao.TeachplanRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@Service
+public class CourseService {
+
+    @Autowired
+    TeachplanRepository teachplanRepository;
+
+    @Autowired
+    CourseBaseRepository courseBaseRepository;
+
+    @Autowired
+    TeachplanMapper teachplanMapper;
+
+    @Autowired
+    CoursePicRepository coursePicRepository;
+
+    //课程计划查询
+    public TeachplanNode findTeachplanList(String courseId){
+        return teachplanMapper.selectList(courseId);
+    }
+
+    //添加课程计划
+    @Transactional
+    public ResponseResult addTeachplan(Teachplan teachplan) {
+        if(teachplan == null ||
+                StringUtils.isEmpty(teachplan.getCourseid()) ||
+                StringUtils.isEmpty(teachplan.getPname())){
+            ExceptionCast.cast(CommonCode.INVALID_PARAM);
+        }
+        //课程id
+        String courseid = teachplan.getCourseid();
+        //页面传入的parentId
+        String parentid = teachplan.getParentid();
+        if(StringUtils.isEmpty(parentid)){
+            //取出该课程的根结点
+            parentid = this.getTeachplanRoot(courseid);
+        }
+        Optional<Teachplan> optional = teachplanRepository.findById(parentid);
+        Teachplan parentNode = optional.get();
+        //父结点的级别
+        String grade = parentNode.getGrade();
+        //新结点
+        Teachplan teachplanNew = new Teachplan();
+        //将页面提交的teachplan信息拷贝到teachplanNew对象中
+        BeanUtils.copyProperties(teachplan,teachplanNew);
+        teachplanNew.setParentid(parentid);
+        teachplanNew.setCourseid(courseid);
+        if(grade.equals("1")){
+            teachplanNew.setGrade("2");//级别，根据父结点的级别来设置
+        }else{
+            teachplanNew.setGrade("3");
+        }
+
+        teachplanRepository.save(teachplanNew);
+
+        return new ResponseResult(CommonCode.SUCCESS);
+    }
+
+    //查询课程的根结点，如果查询不到要自动添加根结点
+    private String getTeachplanRoot(String courseId){
+        Optional<CourseBase> optional = courseBaseRepository.findById(courseId);
+        if(!optional.isPresent()){
+            return null;
+        }
+        //课程信息
+        CourseBase courseBase = optional.get();
+        //查询课程的根结点
+        List<Teachplan> teachplanList = teachplanRepository.findByCourseidAndParentid(courseId, "0");
+        if(teachplanList == null || teachplanList.size()<=0){
+            //查询不到，要自动添加根结点
+            Teachplan teachplan = new Teachplan();
+            teachplan.setParentid("0");
+            teachplan.setGrade("1");
+            teachplan.setPname(courseBase.getName());
+            teachplan.setCourseid(courseId);
+            teachplan.setStatus("0");
+            teachplanRepository.save(teachplan);
+            return teachplan.getId();
+        }
+        //返回根结点id
+        return teachplanList.get(0).getId();
+
+    }
+
+    //向课程管理数据添加课程与图片的关联信息
+    @Transactional
+    public ResponseResult addCoursePic(String courseId, String pic) {
+        //课程图片信息
+        CoursePic coursePic = null;
+        //查询课程图片
+        Optional<CoursePic> picOptional = coursePicRepository.findById(courseId);
+        if(picOptional.isPresent()){
+            coursePic = picOptional.get();
+        }
+        if(coursePic == null){
+            coursePic  = new CoursePic();
+        }
+        coursePic.setPic(pic);
+        coursePic.setCourseid(courseId);
+        coursePicRepository.save(coursePic);
+        return new ResponseResult(CommonCode.SUCCESS);
+    }
+
+    //查询课程图片
+    public CoursePic findCoursePic(String courseId) {
+        //查询课程图片
+        Optional<CoursePic> picOptional = coursePicRepository.findById(courseId);
+        if(picOptional.isPresent()){
+            CoursePic coursePic = picOptional.get();
+            return coursePic;
+        }
+        return null;
+    }
+
+    //删除课程图片
+    @Transactional
+    public ResponseResult deleteCoursePic(String courseId) {
+        //执行删除
+        long result = coursePicRepository.deleteByCourseid(courseId);
+        if(result>0){
+            return new ResponseResult(CommonCode.SUCCESS);
+        }
+        return new ResponseResult(CommonCode.FAIL);
+    }
+}
